afedf96e43d5ba903199528d392c884e6a2ca760
dils <dilsdev55@gmail.com>
2026-03-27 10:47:45 +0700

    fix: perbaiki namespace duplikat ShiftCodeResource Pages

--- app/Filament/Resources/ShiftCodeResource/Pages/ListShiftCodes.php
+++ app/Filament/Resources/ShiftCodeResource/Pages/ListShiftCodes.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Filament\Resources\ShiftCodes\Pages;
+namespace App\Filament\Resources\ShiftCodeResource\Pages;
 
-use App\Filament\Resources\ShiftCodes\ShiftCodeResource;
+use App\Filament\Resources\ShiftCodeResource;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
