02d986fde97bf3fe65d8d2246f904529769e82e8
dils <dilsdev55@gmail.com>
2026-04-24 11:54:50 +0700

    add feauture checked all on menu shift schedules

--- app/Filament/Resources/ShiftScheduleResource.php
+++ app/Filament/Resources/ShiftScheduleResource.php
@@ -133,12 +133,12 @@ class ShiftScheduleResource extends Resource
                     ->searchable(),
             ])
             ->actions([
-                \Filament\Tables\Actions\EditAction::make(),
-                \Filament\Tables\Actions\DeleteAction::make(),
+                \Filament\Actions\EditAction::make(),
+                \Filament\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
-                \Filament\Tables\Actions\BulkActionGroup::make([
-                    \Filament\Tables\Actions\DeleteBulkAction::make()->label('Hapus Terpilih'),
+                \Filament\Actions\BulkActionGroup::make([
+                    \Filament\Actions\DeleteBulkAction::make()->label('Hapus Terpilih'),
                 ]),
             ])
             ->defaultSort('date', 'desc');
